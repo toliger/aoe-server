80e172087e133adaaefb46af970aa7759047f581
arauch <arauch@etu.unistra.fr>
2019-02-06 21:20:33 +0100

    correction du code ressources

--- ressource/ressource.go
+++ ressource/ressource.go
@@ -7,28 +7,31 @@ type Ressource struct{
     x int
     y int
     pv int
-    type int // 0:water, 1:tree, 2:rock, 3 food,
+    typ int // 0:water, 1:tree, 2:rock, 3 food,
 
 }
 //Crée la Ressource
-func New(x int, y int, pv int, type int) Ressource {
-    res:=Ressource{x,y,pv,tp}
-    return res
+func new(x int, y int, pv int, type int) Ressource {
+    return (Ressource{x,y,pv,tp})
 }
 
 func Create(class string, x int, y int) Ressource {
     var res Ressource
     switch class{
         case "water":
-            res=New(x, y, 100,  0)
+            res=new(x, y, 100,  0)
         case "tree":
-            res=New(x, y, 100,  1)
+            res=new(x, y, 100,  1)
         case "rock":
-            res=New(x, y, 100,  2)
+            res=new(x, y, 100,  2)
         case "food":
-            res=New(x, y, 100, 3)
+            res=new(x, y, 100, 3)
         default:
             return -1, errors.New("Class of Ressource not specified correctly")
     }
     return res
+}
+
+func IsHarvestable(Ressource res) bool{
+	return res.typ!="water"
 }
