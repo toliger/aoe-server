7aaf7fa7f5ec568fdf73396980051559591fac4c
dorian <dorian.schwambach@tu.unistra.fr>
2019-02-27 17:00:05 +0100

    methode recolte (premiere version)

--- joueur/joueur.go
+++ joueur/joueur.go
@@ -63,3 +63,61 @@ func (j *Joueur) AddFood(f int){
 func (j *Joueur)AddBuilding(b batiment.Batiment){
 	(*j).batiments=append(j.batiments,b)
 }
+
+func Abs(x int) int {
+	if (x < 0) {
+		return -x
+	}
+	return x
+}
+
+//Recolte de ressources (se deplace vers la ressource la plus proche dans la vue du villageois)
+func (joueur Joueur) recolte(vill npc.Npc, c carte.Carte){
+	var i, j int
+	var ress *ressource.Ressource
+	distance := 2000
+	if (vill.GetType() == 2){
+		fmt.Println("Un soldat ne peut pas recolter de ressources")
+		return
+	}
+	for i = vill.GetX() - vill.GetVue(); i <= vill.GetX() + vill.GetVue(); i++{
+		for j = vill.GetY() - vill.GetVue(); j <= vill.GetY() + vill.GetVue(); j++{
+			if (c.GetTile(i, j).GetType() == 2){
+				if (c.GetTile(i, j).GetRess().GetType() == 2 && vill.GetType() != 0){
+					fmt.Println("Seul un harvester peut recolter de la pierre")
+					continue;
+				}
+				if (Abs(i - vill.GetX()) + Abs(j - vill.GetY()) < distance){
+					distance = Abs(i - vill.GetX()) + Abs(j - vill.GetY())
+					ress = c.GetTile(i, j).GetRess()
+				}
+			}
+		}
+	}
+	// pas de ressources dans la vue du villageois
+	if (distance == 2000){
+		return
+	}
+	go vill.MoveTo(c, ress.GetX(), ress.GetY());
+	var elapsed time.Duration
+	start := time.Now()
+	for{
+		elapsed = time.Since(start)
+		if (elapsed % 1 == 0){
+			// Le villageois ne se trouve pas (ou plus) à l'emplacement de la ressource
+			if (vill.GetX() != ress.GetX() || vill.GetY() != ress.GetY()){
+				break
+			}
+			switch ress.GetType(){
+			case 1:
+				joueur.AddWood(cst.TauxRecolte)
+			case 2:
+				joueur.AddStone(cst.TauxRecolte)
+			case 3:
+				joueur.AddFood(cst.TauxRecolte)
+			default:
+				joueur.AddFood(cst.TauxRecolte)
+			}
+		}
+	}
+}
