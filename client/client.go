2eb5ef16749775124cb71a03655258396b6530f5
Louis THOMANN <loulou1997@gmail.com>
2019-03-05 20:14:53 +0100

    Implémentation des fonctions d'écoute et de réponse gRPC

--- client/client.go
+++ client/client.go
@@ -3,19 +3,69 @@ package client
 
 import (
 	"git.unistra.fr/AOEINT/server/npc"
-	"git.unistra.fr/AOEINT/server/carte"
+	/*"git.unistra.fr/AOEINT/server/carte"
 	"git.unistra.fr/AOEINT/server/ressource"
 	"git.unistra.fr/AOEINT/server/joueur"
-	"git.unistra.fr/AOEINT/server/batiment"
+	"git.unistra.fr/AOEINT/server/batiment"*/
+
+	"fmt"
+	"context"
+	"log"
+	"net"
+	"google.golang.org/grpc"
+	pb "git.unistra.fr/AOEINT/server/serveur"
 )
 
-//serveur vers client
+///////////////////////////////////////////////////////////////////////////////
+// Général
+///////////////////////////////////////////////////////////////////////////////
+
+var server *grpc.Server
+
+type Server struct {}
+
+// Fonction demarrant la gestion des intéractions gRPC
+// Fonction bloquante, à lancer en concurrence
+func InitListenerServer(adress string) {
+
+	// Initialisation du socket d'écoute réseau
+	lis, err := net.Listen("tcp", adress)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	// Initialisation du serveur gRPC
+	server = grpc.NewServer()
+
+	// Enregistement des services Hello, Map et Interactions sur le serveur
+	pb.RegisterHelloServer(server, &Server{})
+	pb.RegisterMapServer(server, &Server{})
+	pb.RegisterInteractionsServer(server, &Server{})
+
+	// Mise en écoute du serveur
+	if err := server.Serve(lis); err != nil {
+		log.Fatalf("failed to serve gRPC: %v", err)
+	}
+}
+
+// Fonction arrêtant la gestion des intéractions gRPC (arrêt propre)
+func StopListenerServer() {
+	server.GracefulStop()
+}
+
+// Fonction arrêtant la gestion des intéractions gRPC (arrêt brutal)
+func KillListenerServer() {
+	server.Stop()
+}
+
+
+///////////////////////////////////////////////////////////////////////////////
+// Serveur -> Client
+///////////////////////////////////////////////////////////////////////////////
 
 //Envoie toutes les donnees necessaires à la mise en place de la partie en debut de jeu
 //A envoyer: donnees des joueurs, structure data(map), entites de depart..
-func initGameState(){
-	
-}
+func InitGameState() {}
 
 //Maj les ressources du joueur à partir de l'uid correspondant
 func updatePlayerRessources(playerUID string,stone int,wood int,food int){}
@@ -30,7 +80,46 @@ func PlayerNpcsDied(playerUID string,npc []npc.Npc){
 
 }
 
-//client vers serveur
+///////////////////////////////////////////////////////////////////////////////
+// Client -> Serveur
+///////////////////////////////////////////////////////////////////////////////
+
+// Fonction du service Hello: SayHello
+func (s *Server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
+	fmt.Println("Reception d'un HelloRequest et envoie d'un HelloReply")
+	return &pb.HelloReply{}, nil
+}
+
+// Fonction du service Map: GetMap
+func (s *Server) GetMap(ctx context.Context, in *pb.GetMapRequest) (*pb.GetMapReply, error) {
+	fmt.Println("Reception d'un GetMapRequest et envoie d'un GetMapReply")
+	return &pb.GetMapReply{}, nil
+}
+
+// Fonction du service Map: SetMap
+func (s *Server) SetMap(ctx context.Context, in *pb.SetMapRequest) (*pb.SetMapReply, error) {
+	fmt.Println("Reception d'un SetMapRequest et envoie d'un SetMapReply")
+	return &pb.SetMapReply{}, nil
+}
+
+// Fonction du service Map: UpdateMap
+func (s *Server) UpdateMap(ctx context.Context, in *pb.UpdateMapRequest) (*pb.UpdateMapReply, error) {
+	fmt.Println("Reception d'un UpdateMapRequest et envoie d'un UpdateMapReply")
+	return &pb.UpdateMapReply{}, nil
+}
+
+
+// Fonction du service Interactions: RightClick
+func (s *Server) RightClick(ctx context.Context, in *pb.RightClickRequest) (*pb.RightClickReply, error) {
+	fmt.Println("Reception d'un RightClickRequest et envoie d'un RightClickReply")
+	return &pb.RightClickReply{}, nil
+}
+
+// Fonction du service Map: MoveTo
+func (s *Server) MoveTo(ctx context.Context, in *pb.MoveToRequest) (*pb.MoveToReply, error) {
+	fmt.Println("Reception d'un MoveToRequest et envoie d'un MoveToReply")
+	return &pb.MoveToReply{}, nil
+}
 
 //demande la creation d'un batiment à partir de l'uid du joueur, une position et un type de batiment
 //class: "auberge","caserne","etabli"
