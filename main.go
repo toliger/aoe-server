35775854fd3f36ec42c45d42ff18677508a315e4
Timothée Oliger <oliger.timothee@gmail.com>
2019-03-09 18:34:14 +0100

    add grpc server

--- main.go
+++ main.go
@@ -1,10 +1,29 @@
 package main
 
-import "fmt"
-import "git.unistra.fr/AOEINT/server/affichage"
-import simulateClient "git.unistra.fr/AOEINT/server/falseclient"
-import "git.unistra.fr/AOEINT/server/game"
-import "git.unistra.fr/AOEINT/server/data"
+import (
+  "fmt"
+  "git.unistra.fr/AOEINT/server/affichage"
+  simulateClient "git.unistra.fr/AOEINT/server/falseclient"
+  "git.unistra.fr/AOEINT/server/game"
+  "git.unistra.fr/AOEINT/server/data"
+
+  "context"
+  "log"
+  "net"
+  "google.golang.org/grpc"
+  pb "git.unistra.fr/AOEINT/server/serveur"
+)
+
+const (
+	port = ":50067"
+)
+
+type server struct{}
+
+func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
+	log.Printf("Received !")
+	return &pb.HelloReply{}, nil
+}
 
 func main() {
 	var g game.Game
@@ -19,4 +38,17 @@ func main() {
 	go simulateClient.StartClient(&(g.GameRunning))
 	affichage.ImprimerCarte(g.Carte)
 	(&g).GameLoop()
+
+  // Listen
+  lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	} else {
+    log.Print("Server listen on ", port)
+  }
+	s := grpc.NewServer()
+	pb.RegisterHelloServer(s, &server{})
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+  }
 }
