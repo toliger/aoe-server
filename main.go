a234f56f2409eb7293db2b3e92a873614d8dc2a2
Arthur <rauch.arthur@gmail.com>
2019-03-13 23:59:49 +0100

    fix

--- main.go
+++ main.go
@@ -18,7 +18,7 @@ func main() {
 	fmt.Println("Data struct extracted from json:",data)
 	fmt.Println("buffer",d.ActionBuffer)
 	// On lance le faux client pour tester les fonctions de liaison
-	(&g).GameLoop()
+	go (&g).GameLoop()
 
 	// Listen
 	client.InitListenerServer(&g)
