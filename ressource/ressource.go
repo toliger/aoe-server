c63d0fc585c915ad9eb03e6c6bee02b2a32efd38
Arthur <rauch.arthur@gmail.com>
2019-03-14 15:40:17 +0100

    Datarace recolte ajout RWMutex

--- ressource/ressource.go
+++ ressource/ressource.go
@@ -11,11 +11,11 @@ type Ressource struct{
     Y int
     Pv int
     Typ int // 0:water, 1:tree, 2:rock, 3 food ...
-	mutex *sync.Mutex
+	mutex *sync.RWMutex
 }
 
 func new(x int, y int, pv int, typ int) Ressource {
-    return (Ressource{x,y,pv,typ,&sync.Mutex{}})
+    return (Ressource{x,y,pv,typ,&sync.RWMutex{}})
 }
 
 //Create : generate a new npc
@@ -77,8 +77,8 @@ func (res Ressource)GetY() int{
 
 //GetPv : return PV
 func (res Ressource)GetPv() int{
-	res.mutex.Lock()
-	defer res.mutex.Unlock()
+	res.mutex.RLock()
+	defer res.mutex.RUnlock()
 	return res.Pv
 }
 
