8ca862680e3fe209849bd1ba0f40939d1774a1dd
Arthur <rauch.arthur@gmail.com>
2019-03-14 18:57:21 +0100

    fix

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import (
   "fmt"
   "git.unistra.fr/AOEINT/server/game"
   d "git.unistra.fr/AOEINT/server/data"
-  client "git.unistra.fr/AOEINT/server/server"
+  "git.unistra.fr/AOEINT/server/server"
 )
 
 func main() {
