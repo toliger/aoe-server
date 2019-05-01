6fdc48d617d474e783e2369d516784f97501a58c
Arthur <rauch.arthur@gmail.com>
2019-05-01 18:27:35 +0200

    Remplacement des bouchons pour les JWT

--- game/game.go
+++ game/game.go
@@ -261,8 +261,10 @@ Modification: Changement pour des valeurs statiques (temporaire)
 */
 func (g *Game) GetPlayerData() {
 	(*g).Joueurs = make([]*joueur.Joueur, 2)
-	j0 := joueur.Create(0, "Bob", "b33d954f-c63e-4b48-88eb-8b5e86d94246")
-	j1 := joueur.Create(1, "Alice", "1982N19N2")
+	id1:= data.ExtractFromToken(constants.Player1JWT).UUID
+	id2:= data.ExtractFromToken(constants.Player2JWT).UUID
+	j0 := joueur.Create(0, "Bob", id1)
+	j1 := joueur.Create(1, "Alice", id2)
 	(*g).Joueurs[0] = &j0
 	(*g).Joueurs[1] = &j1
 	constants.PlayerUID1 = (*g).Joueurs[0].UID
