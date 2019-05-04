b0ccc7fa1c9f8cf579f288dd6241fee7d6fb72a3
Arthur <rauch.arthur@gmail.com>
2019-05-04 17:14:31 +0200

    removing duplicates

--- main.go
+++ main.go
@@ -10,7 +10,6 @@ func main() {
 	d.ExtractFromToken("aubvfauipva.eyJncm91cCI6InBsYXllciIsIm5hbWUiOiJQaWVycmUgQyIsInV1aWQiOiJiMzNkOTU0Zi1jNjNlLTRiNDgtODhlYi04YjVlODZkOTQyNDYiLCJpYXQiOjE1MTYyMzkwMjJ9.oaougf")
 	var g game.Game
 	d.IDMap = d.NewObjectID()
-	d.InitiateActionBuffer()
 	cExit := make(chan(bool))
 	g.GameRunning = cExit
 	(&g).GetPlayerData()
