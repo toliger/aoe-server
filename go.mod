a9e923590b5cd01bf564dbaa949953f441e6f7bf
Timothée Oliger <oliger.timothee@gmail.com>
2019-05-08 12:20:40 +0200

    prom

--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/machinebox/graphql v0.2.2
 	github.com/matryer/is v1.2.0 // indirect
 	github.com/pkg/errors v0.8.1 // indirect
+	github.com/prometheus/client_golang v0.9.2
 	golang.org/x/net v0.0.0-20190313220215-9f648a60d977 // indirect
 	golang.org/x/sys v0.0.0-20190312061237-fead79001313 // indirect
 	golang.org/x/text v0.3.1-0.20180807135948-17ff2d5776d2 // indirect
--- main.go
+++ main.go
@@ -5,9 +5,36 @@ import (
 	"git.unistra.fr/AOEINT/server/game"
 	"git.unistra.fr/AOEINT/server/server"
 	"log"
+  "net/http"
+        "time"
+
+        "github.com/prometheus/client_golang/prometheus"
+        "github.com/prometheus/client_golang/prometheus/promauto"
+        "github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+func recordMetrics() {
+        go func() {
+                for {
+                        opsProcessed.Inc()
+                        time.Sleep(2 * time.Second)
+                }
+        }()
+}
+
+var (
+        opsProcessed = promauto.NewCounter(prometheus.CounterOpts{
+                Name: "players_count",
+                Help: "The total number of players",
+        })
 )
 
 func main() {
+
+  recordMetrics()
+
+  http.Handle("/metrics", promhttp.Handler())
+  go http.ListenAndServe(":2112", nil)
 	d.ExtractFromToken("aubvfauipva.eyJncm91cCI6InBsYXllciIsIm5hbWUiOiJQaWVycmUgQyIsInV1aWQiOiJiMzNkOTU0Zi1jNjNlLTRiNDgtODhlYi04YjVlODZkOTQyNDYiLCJpYXQiOjE1MTYyMzkwMjJ9.oaougf")
 	var g game.Game
 	d.IDMap = d.NewObjectID()
@@ -26,6 +53,8 @@ func main() {
 	log.Println(g.Joueurs[1].UID)
 	server.InitListenerServer(&g)
 	//go initialize(&g)
+
+
 }
 
 func initialize(g *game.Game){
@@ -36,5 +65,9 @@ func initialize(g *game.Game){
 	go g.LaunchAutomaticFight()
 	go g.BrokenBuildingsCollector()
 	// On lance le faux client pour tester les fonctions de liaison
+
 	go g.GameLoop()
+
+
+
 }
